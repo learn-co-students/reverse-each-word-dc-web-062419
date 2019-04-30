1bc9f7f3453f3d131328898e49496d9b51ef0d76
Trevahr Robertson <trevahr@yahoo.com>
2019-04-30 14:21:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,3 +8,12 @@ def reverse_each_word(string)
 b.join(" ")
 end
 
+def reverse_each_word(string)
+
+  a = string.split(" ")
+  b=[]
+  a.each do |word|
+   b.push(word.reverse)
+  end
+b.join(" ")
+end
